dbd5e023f4df0f39cdf82f742951dcbf862ea0ce
Jeff Carnegie <jeff.carnegie@gmail.com>
2014-12-18 15:31:02 -0800

    react layouts

--- templates/client/lib/apiclient.js
+++ templates/client/lib/apiclient.js
@@ -1,8 +1,7 @@
 var r       = require("ramda");
-var env     = require("../../lib/env");
+var config  = require("../../lib/config");
 var request = require("superagent");
 var Promise = require("es6-promise").Promise;
-var config  = env.config();
 
 var apiUrl = function(path) {
     return env.config().api.base + path;
