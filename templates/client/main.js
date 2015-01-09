25adb1978812fb732d3edc2bc88a479bb6a1a13c
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-01-08 17:14:44 -0800

    Fixed issue with webpack bundling multiple versions of react on the client side

--- templates/client/main.js
+++ templates/client/main.js
@@ -7,14 +7,14 @@ var isoreact    = require("isoreact");
 var routesList  = require("./routes");
 var urlUtil     = require("url");
 var querystring = require("querystring");
-var App         = require("./layouts/app.jsx");
+var layout      = require("./layouts/app.jsx");
+var router      = require("isoreact/lib/clientrouter")(react);
 
 var selector   = "body";
-var router     = isoreact.clientrouter;
 var pageAction = router.reactPageAction(selector);
 
 // setup routing
-var routes = r.reduce(router.configureRoute(pageAction, App), router.create(), routesList);
+var routes = r.reduce(router.configureRoute(pageAction, layout), router.create(), routesList);
 
 // route clicks
 // todo: explore if this will be a performance issue - do we need to hook all
@@ -25,4 +25,4 @@ document.addEventListener("click", router.handleClicks(routes));
 window.onpopstate = router.handlePopState(routes);
 
 // init
-document.onload = router.init(routes, selector, window.location.href);
+document.addEventListener("DOMContentLoaded", router.init(routes, selector, window.location.href, layout));
--- templates/server/main.js
+++ templates/server/main.js
@@ -3,21 +3,23 @@ var react           = require("react");
 var morgan          = require("morgan");
 var express         = require("express");
 var isoreact        = require("isoreact");
+var serverroutes    = require("isoreact/lib/serverroutes");
 var routeList       = require("./routes");
 var clientRouteList = require("../client/routes");
 var routeshelper    = require("./lib/routeshelper");
 var htmlComponent   = require("../client/layouts/html.jsx");
 var defaultLayout   = require("../client/layouts/app.jsx");
 
-var serverroutes = isoreact.serverroutes;
-var config       = isoreact.config;
-var app          = express();
+// create express app server
+var app = express();
 
 // logging
 app.use(morgan("dev"));
 
 // configure static directories
 app.use(express.static("dist/public"));
+
+// add your middleware here
 
 // configure server-side api + client routes
 serverroutes.setServerRoutes(app, routeList);
